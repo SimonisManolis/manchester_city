f44e2d4f6bbd9159a08e09855db330deb1010779
SimonisManolis <emmasimo2031@yahoo.gr>
2019-11-18 21:10:47 +0200

    matchesHome section started

---
+++ src/Components/Home/Matches/matchesHome.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Tag } from '../../ui/misc';
+
+const MatchesHome = () =>{
+    return(
+        <div className="home_matches_wrapper">
+            <div className="container">
+                <Tag
+                    
+                    bck="#0e1731"
+                    size="50px"
+                    color="#ffffff"
+                    //additional props
+                    //add={{
+                      //  color:'red'
+                    //}}
+                >
+
+                    Matches
+                </Tag>
+
+                Blocks
+
+                <Tag
+                    link={true}
+                    linkTo="/the_matches"
+                    bck="#ffffff"
+                    size="22px"
+                    color="#0e1731"
+                    //additional props
+                    //add={{
+                      //  color:'red'
+                    //}}
+                >
+
+                    See more matches
+                </Tag>
+
+                
+            </div>
+           
+           
+        </div>
+    )
+}
+
+export default MatchesHome;
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Featured from './Featured/featured';
+import MatchesHome from './Matches/matchesHome';
 
 const Home =()=>{
     return(
         <div className="bck_blue">
             <Featured/>
+            <MatchesHome/>
         </div>
     )
 }
