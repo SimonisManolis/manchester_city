0084c8940cba7c01ba7474d1e525905044449790
SimonisManolis <emmasimo2031@yahoo.gr>
2019-11-21 01:29:41 +0200

    login completed

---
+++ src/Components/SignIn/signIn.js
@@ -0,0 +1,128 @@
+import React , {Component} from 'react';
+import FormField from '../ui/formField';
+import {validate} from '../ui/misc';
+import {firebase} from '../../firebase';
+
+class SignIn extends Component {
+
+    state = {
+        formError: false,
+        formSuccess: '',
+        formdata: {
+            email: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'email_input',
+                    type: 'email',
+                    placeholder: 'Enter your email'
+                },
+                validation: {
+                    required: true,
+                    email: true
+                },
+                valid: false,
+                validationMessage: ''
+            },
+            password: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'password_input',
+                    type: 'password',
+                    placeholder: 'Enter your password'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: ''
+            }
+        }
+        
+    }
+
+    updateForm(element){
+        //dimiourgw antigrafo tou state gia 
+        // na min enimeronetai sunexeia
+        //alla mono sto telos
+        const newFormdata = {...this.state.formdata}
+        const newElement = {...newFormdata[element.id]} 
+
+        //pairnei tin timi apo to field
+        newElement.value = element.event.target.value;
+
+        let validData = validate(newElement);
+        newElement.valid=validData[0];
+        newElement.validationMessage=validData[1]
+        //tin pernaw stin newFormdata 
+        newFormdata[element.id] = newElement;
+
+        this.setState({
+            formError:false,
+            formdata: newFormdata
+        })
+    }
+
+    submitForm(event){
+        event.preventDefault();
+
+        let dataToSubmit= {};
+        let formIsValid = true;
+
+        for(let key in this.state.formdata){
+            dataToSubmit[key] = this.state.formdata[key].value;
+            //elenxos kai sto formIsValid gt prepei na einai ola ta pedia true
+            formIsValid = this.state.formdata[key].valid && formIsValid; 
+        }
+
+        if(formIsValid){
+            firebase.auth()
+            .signInWithEmailAndPassword(
+                dataToSubmit.email,
+                dataToSubmit.password
+            ).then(()=>{
+                this.props.history.push('/dashboard')
+            }).catch(error=>{
+                this.setState({
+                    formError:true
+                })
+            })
+            
+        }else{
+            this.setState({
+                formError:true
+            })
+        }
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="signin_wrapper">
+                    <form onSubmit={(event)=>this.submitForm(event)}>
+                        <h2>Log In</h2>
+                        <FormField
+                                id={'email'}
+                                formdata={this.state.formdata.email}
+                                change={(element)=>this.updateForm(element)}
+                        />
+                        <FormField
+                                id={'password'}
+                                formdata={this.state.formdata.password}
+                                change={(element)=>this.updateForm(element)}
+                        />
+                         {this.state.formError ? 
+                                <div className="error_label">Fields are not valid</div>
+                                 :null}
+                        <button onClick={(event)=>this.submitForm(event)}>Log In</button>
+                    </form>
+
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+export default SignIn;
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Layout from './HOC/layout';
 import {Switch, Route} from 'react-router-dom';
 import Home from './Components/Home/home';
+import SignIn from './Components/SignIn/signIn';
 
 
 function Routes() {
   return (
      <Layout>
        <Switch>
-        <Route exact component={Home} to="/"/>
+        
+        <Route exact component={Home} path="/"/>
+        <Route exact component={SignIn} path="/sign_in"/>
        </Switch>
      </Layout>
     )
